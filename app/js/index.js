35c4d59696b5353075a5026d2bfd916d62f78bfb
jedani <allansichangi35@gmail.com>
2023-05-25 00:15:43 +0300

    barebones structure of the nav and menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,13 @@
 const menuLink = document.querySelector("#menuLink");
+const overlay = document.querySelector('#overlayid');
 
 menuLink.addEventListener('click', () => {
     if (menuLink.classList.contains('open')) {
         menuLink.classList.remove('open');
+        overlay.classList.remove('scroll');
+
     } else {
-        menuLink.classList.add('open')
+        menuLink.classList.add('open');
+        overlay.classList.add('scroll');
     }
-
-
-
 })
