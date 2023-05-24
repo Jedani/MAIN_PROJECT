c86e62a7e191140aceb0d4afbc343209e531fb37
jedani <allansichangi35@gmail.com>
2023-05-24 18:01:09 +0300

    hamburger menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const menuLink = document.querySelector("#menuLink");
+
+menuLink.addEventListener('click', () => {
+    if (menuLink.classList.contains('open')) {
+        menuLink.classList.remove('open');
+    } else {
+        menuLink.classList.add('open')
+    }
+
+
+
+})
