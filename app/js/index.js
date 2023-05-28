723becd56c4410248d641f4ccf81ce97b70162e5
jedani <allansichangi35@gmail.com>
2023-05-28 05:53:15 +0300

    changed js and almost finishing main section

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import generator from './main.js';
+
 import menu from './mobile-menu.js';
 
 
 menu();
-generator();
--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-function generator() {
-    let generate = document.querySelector('.container');
-    for (let i = 0; i <= 30; i++) {
-        let blocks = document.createElement('div');
-        blocks.classList.add('block');
-        generate.appendChild(blocks);
-    }
-}
-export default generator;
