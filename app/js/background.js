edad971dc0d6b45fb92810b877e1f553b5215ebd
jedani <allansichangi35@gmail.com>
2023-05-29 06:13:31 +0300

    added background

--- app/js/background.js
+++ app/js/background.js
@@ -1,1 +1,12 @@
+const light = document.querySelector('.light');
+const grid = document.querySelector('.container');
 
+function lighter() {
+    grid.addEventListener('mousemove', (e) => {
+        light.style.left = '${e.clientX}px';
+        light.style.top = '${e.clientY}px';
+    })
+
+}
+
+export default lighter;
--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import menu from './mobile-menu.js';
 import exchange from "./flying_menu.js";
+import background from "./background.js";
 
+background();
 exchange();
 menu();
