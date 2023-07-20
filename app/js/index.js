8801647ce3d013360e8c89cdcc690ae2296f15e7
jedani <allansichangi35@gmail.com>
2023-07-20 09:41:40 +0300

    finalised structure of the page

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import menu from './toggle-menu.js';
 import exchange from "./flying_menu.js";
+import parallax from "./parallax.js";
 
 menu();
+parallax();
 exchange();
--- app/js/parallax.js
+++ app/js/parallax.js
@@ -1,1 +1,13 @@
+let responsiveText = document.querySelector(".responsiveText");
 
+
+function parallax() {
+    window.addEventListener("scroll", () => {
+        let offsetY = window.scrollY;
+        responsiveText.style.transform = `translateY(${-offsetY * 0.1}px)`
+    })
+
+
+}
+
+export default parallax;
